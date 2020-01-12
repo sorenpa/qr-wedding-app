93ff53a5641728f54a5caf3cc2d71f491a38e9a3
Søren Peter Andersen <spa@kamstrup.com>
2020-01-12 08:51:10 +0100

    Updates

---
+++ precache-manifest.5ee111875c4cc3d3d8dce331bccf3c81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a15f282d59eca53b679442466cb1e08",
+    "url": "/qr-wedding-app/index.html"
+  },
+  {
+    "revision": "d7b301be10dbb129c60f",
+    "url": "/qr-wedding-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d43da27d6d3b2fc0798",
+    "url": "/qr-wedding-app/static/js/2.2316e9b1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/qr-wedding-app/static/js/2.2316e9b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b301be10dbb129c60f",
+    "url": "/qr-wedding-app/static/js/main.2a2fc5bb.chunk.js"
+  },
+  {
+    "revision": "8530ed4b7595b3c7d48c",
+    "url": "/qr-wedding-app/static/js/runtime-main.d0ede189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qr-wedding-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
